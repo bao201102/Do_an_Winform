b6203a0f4745405e3ef2c85c5af87bed0adc4a30
Nguyễn Ngọc Bảo <bao201102@gmail.com>
2022-05-13 20:16:47 +0700

    á

--- Do_an_Winform/PL/Thukho/frm_Nhap.Designer.cs
+++ Do_an_Winform/PL/Thukho/frm_Nhap.Designer.cs
@@ -53,10 +53,10 @@ namespace Do_an_Winform
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(832, 180);
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.label1.Location = new System.Drawing.Point(670, 158);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 19);
+            this.label1.Size = new System.Drawing.Size(117, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã phiếu nhập:";
             // 
@@ -65,10 +65,10 @@ namespace Do_an_Winform
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(832, 241);
+            this.label2.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.label2.Location = new System.Drawing.Point(670, 212);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 19);
+            this.label2.Size = new System.Drawing.Size(119, 21);
             this.label2.TabIndex = 1;
             this.label2.Text = "Ngày tạo phiếu:";
             // 
@@ -77,10 +77,10 @@ namespace Do_an_Winform
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(832, 299);
+            this.label3.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.label3.Location = new System.Drawing.Point(670, 263);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(95, 19);
+            this.label3.Size = new System.Drawing.Size(110, 21);
             this.label3.TabIndex = 2;
             this.label3.Text = "Tên nhân viên:";
             // 
@@ -89,10 +89,10 @@ namespace Do_an_Winform
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(832, 355);
+            this.label4.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.label4.Location = new System.Drawing.Point(670, 312);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 19);
+            this.label4.Size = new System.Drawing.Size(110, 21);
             this.label4.TabIndex = 3;
             this.label4.Text = "Tên sản phẩm:";
             // 
@@ -101,10 +101,10 @@ namespace Do_an_Winform
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(832, 411);
+            this.label5.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.label5.Location = new System.Drawing.Point(670, 362);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 19);
+            this.label5.Size = new System.Drawing.Size(114, 21);
             this.label5.TabIndex = 4;
             this.label5.Text = "Loại sản phẩm:";
             // 
@@ -113,10 +113,10 @@ namespace Do_an_Winform
             this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(832, 474);
+            this.label6.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.label6.Location = new System.Drawing.Point(670, 417);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(91, 19);
+            this.label6.Size = new System.Drawing.Size(103, 21);
             this.label6.TabIndex = 5;
             this.label6.Text = "Nhà sản xuất:";
             // 
@@ -125,10 +125,10 @@ namespace Do_an_Winform
             this.lblTenNV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblTenNV.AutoSize = true;
-            this.lblTenNV.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTenNV.Location = new System.Drawing.Point(938, 299);
+            this.lblTenNV.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.lblTenNV.Location = new System.Drawing.Point(795, 264);
             this.lblTenNV.Name = "lblTenNV";
-            this.lblTenNV.Size = new System.Drawing.Size(59, 19);
+            this.lblTenNV.Size = new System.Drawing.Size(61, 21);
             this.lblTenNV.TabIndex = 8;
             this.lblTenNV.Text = "Tên NV";
             // 
@@ -137,10 +137,10 @@ namespace Do_an_Winform
             this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(1067, 355);
+            this.label8.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.label8.Location = new System.Drawing.Point(908, 313);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(67, 19);
+            this.label8.Size = new System.Drawing.Size(76, 21);
             this.label8.TabIndex = 9;
             this.label8.Text = "Số lượng:";
             // 
@@ -149,10 +149,10 @@ namespace Do_an_Winform
             this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(1067, 411);
+            this.label9.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.label9.Location = new System.Drawing.Point(908, 363);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(59, 19);
+            this.label9.Size = new System.Drawing.Size(68, 21);
             this.label9.TabIndex = 10;
             this.label9.Text = "Đơn giá:";
             // 
@@ -160,11 +160,11 @@ namespace Do_an_Winform
             // 
             this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dateTimePicker1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(938, 235);
+            this.dateTimePicker1.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(795, 207);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(242, 26);
+            this.dateTimePicker1.Size = new System.Drawing.Size(212, 29);
             this.dateTimePicker1.TabIndex = 11;
             // 
             // dataGridViewNhap
@@ -173,9 +173,9 @@ namespace Do_an_Winform
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridViewNhap.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewNhap.Location = new System.Drawing.Point(26, 21);
+            this.dataGridViewNhap.Location = new System.Drawing.Point(23, 19);
             this.dataGridViewNhap.Name = "dataGridViewNhap";
-            this.dataGridViewNhap.Size = new System.Drawing.Size(740, 636);
+            this.dataGridViewNhap.Size = new System.Drawing.Size(622, 510);
             this.dataGridViewNhap.TabIndex = 12;
             // 
             // lblMaPN
@@ -183,20 +183,20 @@ namespace Do_an_Winform
             this.lblMaPN.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lblMaPN.AutoSize = true;
-            this.lblMaPN.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMaPN.Location = new System.Drawing.Point(938, 180);
+            this.lblMaPN.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.lblMaPN.Location = new System.Drawing.Point(795, 159);
             this.lblMaPN.Name = "lblMaPN";
-            this.lblMaPN.Size = new System.Drawing.Size(55, 19);
+            this.lblMaPN.Size = new System.Drawing.Size(57, 21);
             this.lblMaPN.TabIndex = 13;
             this.lblMaPN.Text = "Mã PN";
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(833, 521);
+            this.button1.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.button1.Location = new System.Drawing.Point(674, 408);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 40);
+            this.button1.Size = new System.Drawing.Size(93, 35);
             this.button1.TabIndex = 14;
             this.button1.Text = "Thêm";
             this.button1.UseVisualStyleBackColor = true;
@@ -204,10 +204,10 @@ namespace Do_an_Winform
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(1117, 521);
+            this.button2.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.button2.Location = new System.Drawing.Point(952, 408);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(96, 40);
+            this.button2.Size = new System.Drawing.Size(84, 35);
             this.button2.TabIndex = 15;
             this.button2.Text = "Lưu và in";
             this.button2.UseVisualStyleBackColor = true;
@@ -215,10 +215,10 @@ namespace Do_an_Winform
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(977, 521);
+            this.button3.Font = new System.Drawing.Font("Leelawadee UI", 12F);
+            this.button3.Location = new System.Drawing.Point(815, 408);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(96, 40);
+            this.button3.Size = new System.Drawing.Size(84, 35);
             this.button3.TabIndex = 16;
             this.button3.Text = "Lưu";
             this.button3.UseVisualStyleBackColor = true;
@@ -228,19 +228,19 @@ namespace Do_an_Winform
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Font = new System.Drawing.Font("Leelawadee UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.Red;
-            this.label7.Location = new System.Drawing.Point(942, 48);
+            this.label7.Location = new System.Drawing.Point(799, 42);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(211, 31);
+            this.label7.Size = new System.Drawing.Size(181, 30);
             this.label7.TabIndex = 17;
             this.label7.Text = "Phiếu nhập hàng";
             // 
             // frm_Nhap
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1257, 682);
+            this.ClientSize = new System.Drawing.Size(1075, 551);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -257,7 +257,7 @@ namespace Do_an_Winform
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "frm_Nhap";
             this.Text = "Tạo phiếu nhập";
--- Do_an_Winform/Program.cs
+++ Do_an_Winform/Program.cs
@@ -17,7 +17,7 @@ namespace Do_an_Winform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_Loading());
+            Application.Run(new frm_Nhanvien());
         }
     }
 }
