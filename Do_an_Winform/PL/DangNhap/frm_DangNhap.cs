3882e117f8ee4b581291f31a8c132188118ebeb7
Lồ Sìn Dậu <luoshanyou75588@gmail.com>
2022-05-13 21:25:29 +0700

    Giao dien dang nhap

--- Do_an_Winform/PL/DangNhap/frm_DangNhap.cs
+++ Do_an_Winform/PL/DangNhap/frm_DangNhap.cs
@@ -17,39 +17,49 @@ namespace Do_an_Winform.PL.DangNhap
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        //private void label3_Click(object sender, EventArgs e)
+        //{
+        //    DialogResult result;
+        //    result = MessageBox.Show("Bạn muốn thoát chương trình ?", "Thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        //    if (result == DialogResult.Yes)
+        //    {
+        //        Application.Exit();
+        //    }
+        //}
+
+        //private void btnCancel_Click(object sender, EventArgs e)
+        //{
+        //    DialogResult result;
+        //    result = MessageBox.Show("Bạn muốn thoát chương trình ?", "Thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        //    if (result == DialogResult.Yes)
+        //    {
+        //        Application.Exit();
+        //    }
+        //}
+        //bool seePass = false;
+        //private void button2_Click(object sender, EventArgs e)
+        //{
+        //    seePass = !seePass;
+        //    if (seePass)
+        //    {
+        //        button2.BackgroundImage = Properties.Resources.icons8_eyelashes_2d_32;
+        //        txtPassword.UseSystemPasswordChar = false; ;
+        //    }
+        //    else
+        //    {
+        //        button2.BackgroundImage = Properties.Resources.eye_icon;
+        //        txtPassword.UseSystemPasswordChar = true;
+        //    }
+        //}
+
+        private void frm_DangNhap_Load(object sender, EventArgs e)
         {
-            DialogResult result;
-            result = MessageBox.Show("Bạn muốn thoát chương trình ?", "Thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (result == DialogResult.Yes)
-            {
-                Application.Exit();
-            }
+
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
+        private void btnClose_Click(object sender, EventArgs e)
         {
-            DialogResult result;
-            result = MessageBox.Show("Bạn muốn thoát chương trình ?", "Thoát", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (result == DialogResult.Yes)
-            {
-                Application.Exit();
-            }
-        }
-        bool seePass = false;
-        private void button2_Click(object sender, EventArgs e)
-        {
-            seePass = !seePass;
-            if (seePass)
-            {
-                button2.BackgroundImage = Properties.Resources.icons8_eyelashes_2d_32;
-                txtPassword.UseSystemPasswordChar = false; ;
-            }
-            else
-            {
-                button2.BackgroundImage = Properties.Resources.eye_icon;
-                txtPassword.UseSystemPasswordChar = true;
-            }
+            Close();
         }
     }
 }
