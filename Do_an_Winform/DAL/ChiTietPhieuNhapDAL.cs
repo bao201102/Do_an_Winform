e5700bd4e8bc6c2d6f42206b7a4d5e67b933bce3
Nguyễn Ngọc Bảo <bao201102@gmail.com>
2022-05-18 14:01:12 +0700

    sua lai dabase tiep nha

---
+++ Do_an_Winform/DAL/ChiTietPhieuNhapDAL.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Do_an_Winform.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_an_Winform.DAL
+{
+    class ChiTietPhieuNhapDAL
+    {
+        public static bool Add(ChiTietPhieuNhapDTO phieuNhapDTO)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<ChiTietPhieuNhapDTO, ChiTietPhieuNhap>());
+            var mapper = new Mapper(config);
+            ChiTietPhieuNhap phieuNhap = mapper.Map<ChiTietPhieuNhap>(phieuNhapDTO);
+
+            CHDTEntities1 entities = new CHDTEntities1();
+            entities.ChiTietPhieuNhaps.Add(phieuNhap);
+            return entities.SaveChanges() > 0 ? true : false;
+        }
+    }
+}
