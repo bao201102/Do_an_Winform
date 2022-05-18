7a1d07e4c01c14cf06d573792e7608dd37fcd012
Đoàn Trần Bá Đạt <89367921+doandat8888@users.noreply.github.com>
2022-05-18 16:01:39 +0700

    dsfsfd

--- Do_an_Winform/PL/Nhanvien/UserControlSanPham.Designer.cs
+++ Do_an_Winform/PL/Nhanvien/UserControlSanPham.Designer.cs
@@ -28,22 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControlSanPham));
             this.dgvAllProduct = new Bunifu.Framework.UI.BunifuCustomDataGrid();
-            this.cbKhachHang = new System.Windows.Forms.ComboBox();
+            this.cbGia = new System.Windows.Forms.ComboBox();
             this.lbl1 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbThuongHieu = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.cbTenSP = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.cbLoaiSP = new System.Windows.Forms.ComboBox();
             this.bunifuPanel1 = new Bunifu.UI.WinForms.BunifuPanel();
-            this.btnAddMem = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.btnLoc = new Bunifu.Framework.UI.BunifuThinButton2();
             this.bunifuLabel1 = new Bunifu.UI.WinForms.BunifuLabel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAllProduct)).BeginInit();
             this.bunifuPanel1.SuspendLayout();
@@ -51,31 +51,31 @@
             // 
             // dgvAllProduct
             // 
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            this.dgvAllProduct.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgvAllProduct.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvAllProduct.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.dgvAllProduct.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvAllProduct.BackgroundColor = System.Drawing.Color.Gainsboro;
             this.dgvAllProduct.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgvAllProduct.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.dgvAllProduct.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(122)))), ((int)(((byte)(224)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvAllProduct.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(122)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAllProduct.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgvAllProduct.ColumnHeadersHeight = 40;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Leelawadee UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(226)))), ((int)(((byte)(244)))));
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvAllProduct.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Leelawadee UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(229)))), ((int)(((byte)(226)))), ((int)(((byte)(244)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvAllProduct.DefaultCellStyle = dataGridViewCellStyle3;
             this.dgvAllProduct.DoubleBuffered = true;
             this.dgvAllProduct.EnableHeadersVisualStyles = false;
             this.dgvAllProduct.GridColor = System.Drawing.Color.WhiteSmoke;
@@ -89,14 +89,21 @@
             this.dgvAllProduct.Size = new System.Drawing.Size(698, 328);
             this.dgvAllProduct.TabIndex = 3;
             // 
-            // cbKhachHang
+            // cbGia
             // 
-            this.cbKhachHang.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.cbKhachHang.FormattingEnabled = true;
-            this.cbKhachHang.Location = new System.Drawing.Point(129, 171);
-            this.cbKhachHang.Name = "cbKhachHang";
-            this.cbKhachHang.Size = new System.Drawing.Size(133, 21);
-            this.cbKhachHang.TabIndex = 5;
+            this.cbGia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbGia.FormattingEnabled = true;
+            this.cbGia.Items.AddRange(new object[] {
+            "Từ 500 - 1 triệu",
+            "Từ 1 - 5 triệu",
+            "Từ 5 - 10 triệu",
+            "Từ 10 - 20 triệu",
+            "Từ 20 - 30 triệu",
+            "Từ 30- 50 triệu"});
+            this.cbGia.Location = new System.Drawing.Point(129, 165);
+            this.cbGia.Name = "cbGia";
+            this.cbGia.Size = new System.Drawing.Size(133, 21);
+            this.cbGia.TabIndex = 5;
             // 
             // lbl1
             // 
@@ -120,14 +127,14 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Thương hiệu";
             // 
-            // comboBox1
+            // cbThuongHieu
             // 
-            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(129, 101);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(133, 21);
-            this.comboBox1.TabIndex = 7;
+            this.cbThuongHieu.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbThuongHieu.FormattingEnabled = true;
+            this.cbThuongHieu.Location = new System.Drawing.Point(129, 101);
+            this.cbThuongHieu.Name = "cbThuongHieu";
+            this.cbThuongHieu.Size = new System.Drawing.Size(133, 21);
+            this.cbThuongHieu.TabIndex = 7;
             // 
             // label2
             // 
@@ -157,7 +164,7 @@
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(15, 171);
+            this.label3.Location = new System.Drawing.Point(15, 169);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 17);
             this.label3.TabIndex = 10;
@@ -174,14 +181,14 @@
             this.label4.TabIndex = 11;
             this.label4.Text = "Loại sản phẩm";
             // 
-            // comboBox3
+            // cbLoaiSP
             // 
-            this.comboBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(129, 230);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(133, 21);
-            this.comboBox3.TabIndex = 12;
+            this.cbLoaiSP.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbLoaiSP.FormattingEnabled = true;
+            this.cbLoaiSP.Location = new System.Drawing.Point(129, 230);
+            this.cbLoaiSP.Name = "cbLoaiSP";
+            this.cbLoaiSP.Size = new System.Drawing.Size(133, 21);
+            this.cbLoaiSP.TabIndex = 12;
             // 
             // bunifuPanel1
             // 
@@ -191,44 +198,45 @@
             this.bunifuPanel1.BorderColor = System.Drawing.Color.Gray;
             this.bunifuPanel1.BorderRadius = 3;
             this.bunifuPanel1.BorderThickness = 1;
-            this.bunifuPanel1.Controls.Add(this.btnAddMem);
+            this.bunifuPanel1.Controls.Add(this.btnLoc);
             this.bunifuPanel1.Controls.Add(this.bunifuLabel1);
             this.bunifuPanel1.Controls.Add(this.label1);
-            this.bunifuPanel1.Controls.Add(this.comboBox3);
-            this.bunifuPanel1.Controls.Add(this.comboBox1);
+            this.bunifuPanel1.Controls.Add(this.cbLoaiSP);
+            this.bunifuPanel1.Controls.Add(this.cbThuongHieu);
             this.bunifuPanel1.Controls.Add(this.label4);
             this.bunifuPanel1.Controls.Add(this.label3);
-            this.bunifuPanel1.Controls.Add(this.cbKhachHang);
+            this.bunifuPanel1.Controls.Add(this.cbGia);
             this.bunifuPanel1.Location = new System.Drawing.Point(818, 147);
             this.bunifuPanel1.Name = "bunifuPanel1";
             this.bunifuPanel1.ShowBorders = true;
             this.bunifuPanel1.Size = new System.Drawing.Size(273, 350);
             this.bunifuPanel1.TabIndex = 13;
             // 
-            // btnAddMem
+            // btnLoc
             // 
-            this.btnAddMem.ActiveBorderThickness = 1;
-            this.btnAddMem.ActiveCornerRadius = 20;
-            this.btnAddMem.ActiveFillColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnAddMem.ActiveForecolor = System.Drawing.Color.Black;
-            this.btnAddMem.ActiveLineColor = System.Drawing.SystemColors.ActiveCaption;
-            this.btnAddMem.BackColor = System.Drawing.Color.White;
-            this.btnAddMem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnAddMem.BackgroundImage")));
-            this.btnAddMem.ButtonText = "Lọc";
-            this.btnAddMem.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnAddMem.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddMem.ForeColor = System.Drawing.Color.Black;
-            this.btnAddMem.IdleBorderThickness = 1;
-            this.btnAddMem.IdleCornerRadius = 20;
-            this.btnAddMem.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(213)))), ((int)(((byte)(234)))));
-            this.btnAddMem.IdleForecolor = System.Drawing.Color.Black;
-            this.btnAddMem.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(213)))), ((int)(((byte)(234)))));
-            this.btnAddMem.Location = new System.Drawing.Point(172, 289);
-            this.btnAddMem.Margin = new System.Windows.Forms.Padding(5);
-            this.btnAddMem.Name = "btnAddMem";
-            this.btnAddMem.Size = new System.Drawing.Size(90, 44);
-            this.btnAddMem.TabIndex = 33;
-            this.btnAddMem.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLoc.ActiveBorderThickness = 1;
+            this.btnLoc.ActiveCornerRadius = 20;
+            this.btnLoc.ActiveFillColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLoc.ActiveForecolor = System.Drawing.Color.Black;
+            this.btnLoc.ActiveLineColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLoc.BackColor = System.Drawing.Color.White;
+            this.btnLoc.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLoc.BackgroundImage")));
+            this.btnLoc.ButtonText = "Lọc";
+            this.btnLoc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoc.Font = new System.Drawing.Font("Leelawadee UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoc.ForeColor = System.Drawing.Color.Black;
+            this.btnLoc.IdleBorderThickness = 1;
+            this.btnLoc.IdleCornerRadius = 20;
+            this.btnLoc.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(213)))), ((int)(((byte)(234)))));
+            this.btnLoc.IdleForecolor = System.Drawing.Color.Black;
+            this.btnLoc.IdleLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(213)))), ((int)(((byte)(234)))));
+            this.btnLoc.Location = new System.Drawing.Point(172, 289);
+            this.btnLoc.Margin = new System.Windows.Forms.Padding(5);
+            this.btnLoc.Name = "btnLoc";
+            this.btnLoc.Size = new System.Drawing.Size(90, 44);
+            this.btnLoc.TabIndex = 33;
+            this.btnLoc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLoc.Click += new System.EventHandler(this.btnLoc_Click);
             // 
             // bunifuLabel1
             // 
@@ -271,17 +279,17 @@
         #endregion
 
         private Bunifu.Framework.UI.BunifuCustomDataGrid dgvAllProduct;
-        private System.Windows.Forms.ComboBox cbKhachHang;
+        private System.Windows.Forms.ComboBox cbGia;
         private System.Windows.Forms.Label lbl1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbThuongHieu;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cbTenSP;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox cbLoaiSP;
         private Bunifu.UI.WinForms.BunifuPanel bunifuPanel1;
         private Bunifu.UI.WinForms.BunifuLabel bunifuLabel1;
-        private Bunifu.Framework.UI.BunifuThinButton2 btnAddMem;
+        private Bunifu.Framework.UI.BunifuThinButton2 btnLoc;
     }
 }
