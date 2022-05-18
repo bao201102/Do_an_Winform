484143603f791ca042e8b5961ea40ab05bccd428
kieupie <92916187+kieupie@users.noreply.github.com>
2022-05-18 19:16:58 +0700

    fix
    fix

--- Do_an_Winform/PL/Thukho/frm_Nhap.cs
+++ Do_an_Winform/PL/Thukho/frm_Nhap.cs
@@ -1,6 +1,5 @@
 ﻿using Do_an_Winform.BLL;
 using Do_an_Winform.DTO;
-using Do_an_Winform.PL.DangNhap;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -19,17 +18,7 @@ namespace Do_an_Winform.PL.Thukho
         public frm_Nhap(TaiKhoanDTO user)
         {
             InitializeComponent();
-            gridview_Load();
             taikhoan = user;
-
-            gridviewNhap.AllowUserToResizeColumns = false;
-            gridviewNhap.AllowUserToResizeRows = false;
-            gridviewNhap.MultiSelect = false;
-            gridviewNhap.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            gridviewNhap.Columns[0].ReadOnly = true;
-            gridviewNhap.Columns[2].ReadOnly = true;
-            gridviewNhap.Columns[3].ReadOnly = true;
-            gridviewNhap.Columns[4].ReadOnly = true;
         }
 
         private void frm_Nhap_Load(object sender, EventArgs e)
@@ -45,6 +34,9 @@ namespace Do_an_Winform.PL.Thukho
             cbbTenSP.DisplayMember = "TenSP";
             cbbTenSP.ValueMember = "MaSP";
 
+            txtSoLuong.Text = "1";
+
+            //lblTong.Text = (int.Parse(lblDongia.Text) * int.Parse(txtSoLuong.Text)).ToString();
             lblDongia.Text = SanPhamBLL.GetProductByName(cbbTenSP.Text.ToString()).DonGia.ToString();
         }
 
@@ -57,7 +49,7 @@ namespace Do_an_Winform.PL.Thukho
         private void cbbTenSP_SelectedValueChanged(object sender, EventArgs e)
         {
             lblDongia.Text = SanPhamBLL.GetProductByName(cbbTenSP.Text.ToString()).DonGia.ToString();
-            txtSoLuong.Text = "";
+            //lblTong.Text = (int.Parse(lblDongia.Text) * int.Parse(txtSoLuong.Text)).ToString();
         }
 
         private void cbbTenSP_TextChanged(object sender, EventArgs e)
@@ -65,7 +57,6 @@ namespace Do_an_Winform.PL.Thukho
             try
             {
                 lblDongia.Text = SanPhamBLL.GetProductByName(cbbTenSP.Text.ToString()).DonGia.ToString();
-                txtSoLuong.Text = "";
             }
             catch (Exception)
             {
@@ -81,59 +72,6 @@ namespace Do_an_Winform.PL.Thukho
             catch (Exception)
             {
             }
-        }
-
-        private DataTable dataTable = new DataTable();
-        private void gridview_Load()
-        {
-            dataTable.Columns.Add("Tên sản phẩm");
-            dataTable.Columns.Add("Số lượng");
-            dataTable.Columns.Add("Đơn giá");
-            dataTable.Columns.Add("Tên nhà sản xuất");
-            dataTable.Columns.Add("Thành tiền");
-
-            gridviewNhap.DataSource = dataTable;
-        }
-
-        private int sum = 0;
-        private void btnThem_Click(object sender, EventArgs e)
-        {
-            SanPhamDTO spById = SanPhamBLL.GetProductById(cbbTenSP.SelectedValue.ToString());
-            NhaSanXuatDTO nsx = NhaSanXuatBLL.GetManufacById(spById.MaNhaSX);
-            if (txtSoLuong.Text == "")
-            {
-                bunifuSnackbar1.Show(this, "Chưa nhập số lượng. Vui lòng thử lại!", Bunifu.UI.WinForms.BunifuSnackbar.MessageTypes.Warning);
-            }
-            else
-            {
-                sum += int.Parse(lblTong.Text);
-                lblThanhToan.Text = sum.ToString();
-                dataTable.Rows.Add(cbbTenSP.Text, txtSoLuong.Text, lblDongia.Text, nsx.TenNhaSX, lblTong.Text);
-            }
-            txtSoLuong.Clear();
-        }
-
-        private void btnXoa_Click(object sender, EventArgs e)
-        {
-            if (gridviewNhap.SelectedRows.Count > 0)
-            {
-                MessBox messBox = new MessBox();
-                bool result = messBox.ShowMess("Bạn có muốn xóa sản phẩm này không ?");
-
-                if (result)
-                {
-                    int thanhtien = int.Parse(gridviewNhap.SelectedRows[0].Cells[4].Value.ToString());
-                    sum -= thanhtien;
-                    lblThanhToan.Text = sum.ToString();
-                    gridviewNhap.Rows.RemoveAt(gridviewNhap.SelectedRows[0].Index);
-                }
-            }
-        }
-
-        private void gridviewNhap_CellEndEdit(object sender, DataGridViewCellEventArgs e)
-        {
-            gridviewNhap.SelectedRows[0].Cells[4].Value = int.Parse(gridviewNhap.SelectedRows[0].Cells[1].Value.ToString())
-                * int.Parse(gridviewNhap.SelectedRows[0].Cells[2].Value.ToString());
         }
     }
 }
