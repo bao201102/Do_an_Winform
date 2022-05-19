68ce77f094d908b29ad333c15eb75ee1077be8ef
Lồ Sìn Dậu <luoshanyou75588@gmail.com>
2022-05-19 21:43:30 +0700

    Insert product

--- Do_an_Winform/PL/Quanly/frm_QLSP.Designer.cs
+++ Do_an_Winform/PL/Quanly/frm_QLSP.Designer.cs
@@ -44,6 +44,7 @@
             this.dgvDSSP = new Bunifu.UI.WinForms.BunifuDataGridView();
             this.btnInsert = new Bunifu.Framework.UI.BunifuThinButton2();
             this.btnCreateRP = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.mainPanel = new Bunifu.UI.WinForms.BunifuPanel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvDSSP)).BeginInit();
             this.SuspendLayout();
             // 
@@ -275,6 +276,7 @@
             this.btnInsert.Size = new System.Drawing.Size(100, 50);
             this.btnInsert.TabIndex = 12;
             this.btnInsert.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
             // 
             // btnCreateRP
             // 
@@ -301,6 +303,21 @@
             this.btnCreateRP.TabIndex = 13;
             this.btnCreateRP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // mainPanel
+            // 
+            this.mainPanel.BackgroundColor = System.Drawing.Color.Transparent;
+            this.mainPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mainPanel.BackgroundImage")));
+            this.mainPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.mainPanel.BorderColor = System.Drawing.Color.Transparent;
+            this.mainPanel.BorderRadius = 3;
+            this.mainPanel.BorderThickness = 1;
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.ShowBorders = true;
+            this.mainPanel.Size = new System.Drawing.Size(1328, 844);
+            this.mainPanel.TabIndex = 14;
+            // 
             // frm_QLSP
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -313,6 +330,7 @@
             this.Controls.Add(this.cbLoaisp);
             this.Controls.Add(this.txtFind);
             this.Controls.Add(this.bunifuLabel1);
+            this.Controls.Add(this.mainPanel);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frm_QLSP";
@@ -333,5 +351,6 @@
         private Bunifu.UI.WinForms.BunifuTextBox txtFind;
         private Bunifu.Framework.UI.BunifuThinButton2 btnInsert;
         private Bunifu.Framework.UI.BunifuThinButton2 btnCreateRP;
+        private Bunifu.UI.WinForms.BunifuPanel mainPanel;
     }
 }
